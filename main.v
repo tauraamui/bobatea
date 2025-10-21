358df9af51032f1eb0e446785791866fb0220482
tauraamui <adampstringer@protonmail.com>
2025-10-21 02:06:55 +0100

    tidy: remove now unneeded test state from example

--- main.v
+++ main.v
@@ -10,14 +10,11 @@ enum SessionState as u8 {
 
 struct MainModel {
 mut:
-	pootang []int
     state SessionState
-	event_count int
 }
 
 fn new_model(mut nums []int) MainModel {
     return MainModel{
-        pootang: nums
         state: .timer
     }
 }
@@ -27,8 +24,6 @@ fn (mut m MainModel) init() ?tea.Cmd {
 }
 
 fn (mut m MainModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
-	m.event_count += 1
-
     if msg is tea.KeyMsg {
         if msg.code == .escape {
             return MainModel{}, tea.quit
@@ -38,16 +33,6 @@ fn (mut m MainModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
 	        m.state = if m.state == .timer { .spinner } else { .timer }
 	        return m.clone(), none
         }
-
-		if msg.code == .a {
-			m.event_count = 0
-			return m.clone(), none
-		}
-
-		if msg.code == .x {
-			m.pootang[1] = 222
-			return m.clone(), none
-		}
     }
 
 	return m.clone(), none
@@ -58,7 +43,7 @@ fn (m MainModel) view(mut ctx draw.Contextable) {
     win_height := ctx.window_height()
 
 	state := if m.state == .timer { "timer" } else { "spinner" }
-    msg := "welcome to boba tea! ${state} -> ${m.event_count} (${m.pootang})"
+    msg := "welcome to boba tea! ${state}"
     ctx.draw_text((win_width / 2) - (msg.len / 2), win_height / 2, msg)
 }
 
