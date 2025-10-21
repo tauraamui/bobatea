7f041ac72da7a1c771fd028084724b209f177b37
tauraamui <adampstringer@protonmail.com>
2025-10-21 01:22:18 +0100

    fix: prevent weird self circular referential state

--- main.v
+++ main.v
@@ -9,9 +9,9 @@ enum SessionState as u8 {
 }
 
 struct MainModel {
-    state SessionState
 mut:
-	rendered_count int
+    state SessionState
+	event_count int
 }
 
 fn (mut m MainModel) init() ?tea.Cmd {
@@ -22,23 +22,40 @@ fn (mut m MainModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
     // NOTE(tauraamui): have to create manual non-mutable copy of the final
     // returned model instance in order to not encounter catastrophic C level
     // compiler panic
-	m.rendered_count += 1
-    i_m := m
+	m.event_count += 1
 
     if msg is tea.KeyMsg {
         if msg.code == .escape {
-            return i_m, tea.quit
+            return MainModel{}, tea.quit
         }
+
+        if msg.code == .tab {
+	        m.state = if m.state == .timer { .spinner } else { .timer }
+			return MainModel{
+				state: m.state
+				event_count: m.event_count
+			}, none
+        }
+
+		if msg.code == .a {
+			m.event_count = 0
+			return MainModel{
+				state: m.state
+				event_count: m.event_count
+			}, none
+		}
     }
 
+    i_m := m
     return i_m, none
 }
 
-fn (m MainModel) view(mut ctx draw.Contextable) {
+fn (mut m MainModel) view(mut ctx draw.Contextable) {
     win_width := ctx.window_width()
     win_height := ctx.window_height()
 
-    msg := "welcome to boba tea! ${m.rendered_count}"
+	state := if m.state == .timer { "timer" } else { "spinner" }
+    msg := "welcome to boba tea! ${state} -> ${m.event_count}"
     ctx.draw_text((win_width / 2) - (msg.len / 2), win_height / 2, msg)
 }
 
