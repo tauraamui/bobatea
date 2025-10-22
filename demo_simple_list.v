9368b04d20b3bd27276b722c443b7be57308adc9
tauraamui <adampstringer@protonmail.com>
2025-10-22 14:43:21 +0100

    chore(demo): basic/simple list example

--- demo_simple_list.v
+++ demo_simple_list.v
@@ -52,9 +52,20 @@ fn (mut m SimpleListModel) update(msg tea.Msg) (tea.Model, ?tea.Cmd) {
 }
 
 fn (m SimpleListModel) view(mut ctx tea.Context) {
-    for i, y in m.items {
-        ctx.draw_text(0, i, y)
+    ctx.push_offset(tea.Offset{ x: 2, y: 1 })
+    ctx.draw_text(0, 0, "What do you want for dinner?")
+    ctx.push_offset(tea.Offset{ x: 1, y: 2 })
+    for y, item in m.items {
+        if y == m.selected_index {
+            ctx.set_color(tea.Color.ansi(170))
+            ctx.draw_text(0, y, ">")
+        }
+        ctx.draw_text(2, y, "${y + 1}. ${item}")
+        ctx.reset_color()
     }
+
+    ctx.pop_offset()
+    ctx.pop_offset()
 }
 
 fn (m SimpleListModel) clone() tea.Model {
