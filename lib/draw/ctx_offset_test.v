f060706bc4d6c72b4b0f767a798a39ce839b761f
tauraamui <adampstringer@protonmail.com>
2025-10-27 09:50:44 +0000

    test(offset): ensure push/pop and clear from operations work correctly

--- lib/draw/ctx_offset_test.v
+++ lib/draw/ctx_offset_test.v
@@ -23,12 +23,31 @@ fn test_context_sequential_offset_last_on_first_off() {
 	}
 	t_ctx.setup_grid()!
 
+	mut xx, mut yy := apply_offsets(t_ctx.offsets, 0, 0)
+	assert xx == 0
+	assert yy == 0
+
 	t_ctx.push_offset(Offset{ x: 1, y: 1 })
+
+	xx, yy = apply_offsets(t_ctx.offsets, 0, 0)
+	assert xx == 1
+	assert yy == 1
+
 	t_ctx.push_offset(Offset{ x: 2, y: 2 })
+
+	xx, yy = apply_offsets(t_ctx.offsets, 0, 0)
+	assert xx == 3
+	assert yy == 3
+
 	t_ctx.push_offset(Offset{ x: 5, y: 5 })
+
+	xx, yy = apply_offsets(t_ctx.offsets, 0, 0)
+	assert xx == 8
+	assert yy == 8
+
 	t_ctx.push_offset(Offset{ x: 8, y: 8 })
 
-	mut xx, mut yy := apply_offsets(t_ctx.offsets, 0, 0)
+	xx, yy = apply_offsets(t_ctx.offsets, 0, 0)
 	assert xx == 16
 	assert yy == 16
 
@@ -55,5 +74,30 @@ fn test_context_sequential_offset_last_on_first_off() {
 	xx, yy = apply_offsets(t_ctx.offsets, 0, 0)
 	assert xx == 0
 	assert yy == 0
+}
+
+fn test_context_offset_clear_from_offset() {
+	mut t_ctx := Context{
+		ref: unsafe { nil }
+	}
+	t_ctx.setup_grid()!
+
+	t_ctx.push_offset(Offset{ x: 1, y: 1 })
+	t_ctx.push_offset(Offset{ x: 2, y: 2 })
+	bookmark_id := t_ctx.push_offset(Offset{ x: 5, y: 5 })
+	assert t_ctx.map_id_to_index(bookmark_id)? == 2 // ensure correct index resolved from id lookup
+	t_ctx.push_offset(Offset{ x: 8, y: 8 })
+	t_ctx.push_offset(Offset{ x: 11, y: 11 })
+
+	// NOTE(tauraamui): pre-clearing offsets from given id, they all exist and apply
+	mut xx, mut yy := apply_offsets(t_ctx.offsets, 0, 0)
+	assert xx == 27
+	assert yy == 27
+
+	t_ctx.clear_from_offset(bookmark_id)
+
+	xx, yy = apply_offsets(t_ctx.offsets, 0, 0)
+	assert xx == 3
+	assert yy == 3
 }
 
