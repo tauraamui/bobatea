371ace00386c2b2f6bfe263d5e1333f3eb96c832
tauraamui <adampstringer@protonmail.com>
2025-10-30 23:18:15 +0000

    feat: complete special key set

--- key.v
+++ key.v
@@ -20,7 +20,29 @@ pub fn (k KeyMsg) string() string {
 	return "${prefix}${k.runes.string()}"
 }
 
-const special_keycodes = [tui.KeyCode.tab, .enter, .escape, .space, .backspace]
+const special_keycodes = [
+	tui.KeyCode.tab,
+	.enter, .escape,
+	.space, .backspace,
+	.insert, .delete,
+	.up, .down,
+	.right, .left,
+	.page_up, .page_down,
+	.home, .end,
+	.f1, .f2,
+	.f3, .f4,
+	.f5, .f6,
+	.f7, .f8,
+	.f9, .f10,
+	.f11, .f12,
+	.f13, .f14,
+	.f15, .f16,
+	.f17, .f18,
+	.f19, .f20,
+	.f21, .f22,
+	.f22, .f23,
+	.f24
+]
 
 fn resolve_key_msg(e draw.Event) KeyMsg {
 	// if modifiers is either ctrl or shift then the only character rep field we want to pay attention to
@@ -35,13 +57,12 @@ fn resolve_key_msg(e draw.Event) KeyMsg {
 }
 
 fn code_to_str(code tui.KeyCode, fallback string, is_special bool) string {
-	v := match code {
+	return match code {
 		.null { fallback }
 		else  {
 			code_str := if is_special { code.str() } else { u8(code).ascii_str() }
 			if code_str == "unknown enum value" { fallback } else { code_str }
 		}
 	}
-	return v
 }
 
